0edfc1c4b200652997c9afaffb1d31e7324fa30d
puccia <puccia@bb7253a0-4040-0410-9f55-73e31eb90c18>
2007-12-23 18:54:03 +0000

    New mechanism for handling settings

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name = "BankPassWeb",
+    version = "0.1",
+    packages = find_packages(),
+    exclude_package_data = { '': ['*/local_*.py',]},
+
+    author = "Emanuele Pucciarelli",
+    author_email = "ep@acm.org",
+    description = "A Python interface to the BankPass Web e-commerce system",
+    keywords = "bankpass",
+    url = "http://code.google.com/p/py-bankpassweb",
+    license = "GPL",
+
+)
